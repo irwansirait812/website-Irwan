52ac081bc5369b5b3890c7e124f6cac64c983937
irwan sirait <irwansirait812@gmail.com>
2024-07-19 13:43:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah dikirim!');
+    // Anda dapat menambahkan fungsi untuk mengirim form ke server di sini
+});
